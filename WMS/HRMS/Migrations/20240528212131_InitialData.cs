6c60c914bee4265c61996e3f560bcc35ac566606
urvashij <urvashij@IDR-CPU-009>
2026-04-03 06:07:26 -0700

    rename Solution name

--- WMS/HRMS/Migrations/20240528212131_InitialData.cs
+++ WMS/HRMS/Migrations/20240528212131_InitialData.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
 #pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
 
-namespace QuickStart.Migrations
+namespace HRMS.Migrations
 {
     /// <inheritdoc />
     public partial class InitialData : Migration
